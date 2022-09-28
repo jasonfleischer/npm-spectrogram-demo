8a976d254d970757e76599ca7486ad742090cb65
Jason Fleischer <jason_fleischer@hotmail.ca>
2022-09-28 18:00:59 -0400

    update

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ document.getElementById("resume").onclick = function() {
 document.getElementById("pause").onclick = function() {
 	
 }*/
+// STEP 1. create spectrogram
 const Spectrogram = require("@jasonfleischer/spectrogram");
 
 var spectrogram = new Spectrogram(id = "your_spectrogram_id", useHeatMapColors = true, highlightPeaks = false, darkMode = true, minimumFrequency = 0, maximumFrequency = 22050 );
@@ -30,25 +31,24 @@ function onStartClickEvent(){
 	analyzerNode.fftSize = 1024;
 
 	// STEP 3. request microphone access
-	/*navigator.mediaDevices.getUserMedia({ video: false, audio: true })
+	navigator.mediaDevices.getUserMedia({ video: false, audio: true })
 	  	.then( (mediaStreamObj) => {
 	  		onStreamAquired(mediaStreamObj, analyzerNode);
 		})
 		.catch( (err) => {
 		 	console.log("getUserMedia: " + err);
-		});*/
+		});
 
 	// ---- OR ----
 
 	// STEP 3. setup audio element
-	var audioElement = document.createElement("AUDIO");
+	/*var audioElement = document.createElement("AUDIO");
 	audioElement.src = "audio/your_audio_file.mp3";		
 	audioElement.autoplay = true;	
-	//audio_controller.audioElement.loop = true;
 	audioElement.oncanplay = function () { 
 		var mediaStreamObj = audioElement.captureStream();
 		onStreamAquired(mediaStreamObj, analyzerNode);
-	}
+	}*/
 }
 
 // STEP 4. connect spectrogram
@@ -57,5 +57,10 @@ function onStreamAquired(mediaStreamObj, analyzerNode) {
 	sourceNode.connect(analyzerNode);
 	spectrogram.draw(analyzerNode, audioContext.sampleRate);
 }
+
+// Additional commands
+
+/*spectrogram.resume();
+spectrogram.pause();*/
 
 
