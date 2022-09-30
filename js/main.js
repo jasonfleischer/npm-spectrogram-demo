8033f0783efddfcc1c07456e5a2c585014fc5601
Jason Fleischer <jason_fleischer@hotmail.ca>
2022-09-30 12:46:17 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,18 +1,185 @@
-/*const pianoKit = require("@jasonfleischer/spectrogram");
+const log = require("@jasonfleischer/log");
+const Spectrogram = require("@jasonfleischer/spectrogram");
 
-//const log = require("@jasonfleischer/log");
+var spectrogram = {};
+var audio_controller = {};
+var audio_element_audio_controller = {};
 
-
-
-document.getElementById("start").onclick = function() { 
-	
-};
-document.getElementById("resume").onclick = function() {
-	
+var model = {
+	is_colored: true,
+	is_dark_mode: true,
+	volume_percent: 100,
+	max_frequency:22050,
+	fft_size: 1024
 }
-document.getElementById("pause").onclick = function() {
+
+init = function() {
+
+	spectrogram = new Spectrogram("spectrogram", 
+									useHeatMapColors = model.is_colored, 
+									highlightPeaks = !model.is_colored, 
+									darkMode = model.is_dark_mode,
+									minimumFrequency = 0, 
+									maximumFrequency = model.max_frequency);
+
+	audio_controller = new AudioController(	
+		onStateChange = onAudioStateChanged, 
+		startVisualization = startVisualization, 
+		fftSize = model.fft_size);
+
+	var audioElement = document.createElement("AUDIO");
+	audioElement.src = "audio/your_audio_file.mp3";		
+	audioElement.autoplay = true;
+	audioElement.loop = true;
+	audio_element_audio_controller = new AudioController(	
+		onStateChange = onAudioStateChanged, 
+		startVisualization = startVisualization, 
+		fftSize = model.fft_size,
+		audioElement = audioElement);
 	
-}*/
+	setup_controls();
+	updateUI_buttons(audio_controller.state);
+}
+
+onAudioStateChanged = function(audio_state) {
+	switch(audio_state){
+		case audio_controller_state.STOPPED:
+			break;
+		case audio_controller_state.RESUMED:
+			spectrogram.resume();
+			break;
+		case audio_controller_state.PAUSED:
+			spectrogram.pause();
+			break;
+	}
+	updateUI_buttons(audio_state);
+}
+
+updateUI_buttons = function(audio_state) {
+	switch(audio_state){
+		case audio_controller_state.STOPPED:
+			$("start").disabled = false;
+			$("audio_element_start").disabled = false;
+			$("resume").disabled = true;
+			$("pause").disabled = true;
+			break;
+		case audio_controller_state.RESUMED:
+			$("start").disabled = true;
+			$("audio_element_start").disabled = true;
+			$("resume").disabled = true;
+			$("pause").disabled = false;
+			break;
+		case audio_controller_state.PAUSED:
+			$("start").disabled = true;
+			$("audio_element_start").disabled = true;
+			$("resume").disabled = false;
+			$("pause").disabled = true;
+			break;
+	}
+}
+
+function startVisualization(audio_controller) {
+	spectrogram.draw(audio_controller.analyzerNode, audio_controller.ctx.sampleRate);
+}
+
+function setup_controls(){
+
+	setupOnClicks();
+	function setupOnClicks(){
+		$("start").onclick = function() { audio_controller.start(); };
+		$("resume").onclick = function() { audio_controller.resume(); };
+		$("pause").onclick = function() { audio_controller.pause(); };
+		$("audio_element_start").onclick = function() { audio_element_audio_controller.start(); };
+	}
+
+	setupMaxFrequencySlider();
+	function setupMaxFrequencySlider() {
+		const base_id = "maxFrequency";
+		var slider = $(base_id+"Range");
+		slider.value = model.max_frequency;
+		var sliderText = $(base_id);
+		sliderText.innerHTML = "Max Freq: " + Number(slider.value).toFixed() + "Hz";
+		slider.oninput = function() {
+			var value = Number(this.value);
+			model.max_frequency = value;
+			sliderText.innerHTML = "Max Freq: " + value.toFixed() + "Hz";
+			updateMaxFrequency(value);
+		}
+
+		function updateMaxFrequency(maximumFrequency) {
+			var savedState = audio_controller.state;
+			audio_controller.pause();
+			spectrogram.updateMaximumFrequency(maximumFrequency);
+			spectrogram.refreshCanvasHeight();	
+			if(savedState == audio_controller_state.RESUMED){
+				audio_controller.resume();
+			}
+		}
+	}
+
+	setupCheckboxControls();
+	function setupCheckboxControls(){
+		setupIsColoredSwitch();
+		function setupIsColoredSwitch() { 
+			$("colors_checkbox").addEventListener("change", function(e){
+				var value = this.checked;
+				log.i("on colors change: " + value);
+				model.is_colored = value;
+				spectrogram.updateColors(value);
+				spectrogram.updateHighlightPeaks(!value);			
+			});
+			$("colors_checkbox").checked = model.is_colored;
+		}
+		setupDarkModeSwitch();
+		function setupDarkModeSwitch() { 
+			$("darkmode_checkbox").addEventListener("change", function(e){
+				var value = this.checked;
+				log.i("on darkmode change: " + value);
+				model.is_dark_mode = value;
+				//spectrogram.updateColors(value);
+				//spectrogram.updateHighlightPeaks(!value);			
+			});
+			$("darkmode_checkbox").checked = model.is_dark_mode;
+		}
+	}
+
+	setupSelectControls();
+	function setupSelectControls(){
+		setupFFTSizeSelect();
+		function setupFFTSizeSelect() {
+			const id = "fft_size_select";
+			var select = $(id);
+		
+			$(id).addEventListener("change", function(e){
+				var value = parseInt(this.value);
+				log.i("on "+id+": " + value);
+				model.note_type = value;
+				model.fft_size = value;
+				updateFFTSize(value);
+			});
+			$(id).value = model.fft_size;
+		}
+
+		function updateFFTSize(fftSize) {
+
+			var savedState = audio_controller.state;
+			audio_controller.pause();
+			audio_controller.fftSize = fftSize;
+			audio_controller.analyzerNode.fftSize = fftSize;
+			spectrogram.refreshCanvasHeight();
+
+			if(savedState == audio_controller_state.RESUMED){
+				audio_controller.resume();
+			}
+		}
+	}
+}
+
+$ = function(id){ return document.getElementById(id); }
+
+init();
+///////
+/*
 // STEP 1. create spectrogram
 const Spectrogram = require("@jasonfleischer/spectrogram");
 
@@ -37,7 +204,7 @@ function onStartClickEvent(){
 		})
 		.catch( (err) => {
 		 	console.log("getUserMedia: " + err);
-		});*/
+		});* /
 
 	// ---- OR ----
 
